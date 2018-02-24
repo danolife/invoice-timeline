0b5d447cd0f2d6f21d9bffd909e2fb06bf9a7f03
Dany Badr <badr.dany@gmail.com>
2018-02-24 11:26:46 +0100

    Completed GQL Schema

--- back/schema.js
+++ back/schema.js
@@ -5,15 +5,53 @@ const typeDefs = `
 type Invoice {
   id: ID
   reference: String
-  status: String
-  dueDate: String
-  creationDate: String
+  dueAt: String
+  createdAt: String
   amount: Int
   customer: Customer
+  statuses: [Status]
+  comments: [Comment]
+  payments: [Payment]
+  reminders: [Reminder]
 }
 type Customer {
+  id: ID
   name: String
   picture: String
+}
+type Comment {
+  id: ID
+  createdAt: String
+  createdBy: User
+  body: String
+}
+type Payment {
+  id: ID
+  createdAt: String
+  amount: Int
+}
+type Reminder {
+  id: ID
+  createdAt: String
+  createdBy: User
+  subject: String
+  body: String
+}
+type Status {
+  id: ID
+  status: StatusEnum
+  createdAt: String
+}
+type User {
+  id: ID
+  name: String
+  picture: String
+}
+enum StatusEnum {
+  DUE
+  OVERDUE
+  PAID
+  DISPUTE
 }
 type Query {
   invoices: [Invoice]
