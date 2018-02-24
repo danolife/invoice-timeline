5c03947a95343e71e88466631b3af2dc53eabff8
Dany Badr <badr.dany@gmail.com>
2018-02-24 19:51:50 +0100

    Extracted layout style in order to create a new nextjs page

--- front/pages/index.js
+++ front/pages/index.js
@@ -1,9 +1,9 @@
 import InvoiceList from "../components/InvoiceList";
-import css from "./index.scss";
+import css from "../style/layout.scss";
 import withData from "../lib/withData";
 
 const index = () => (
-  <div className={css.index}>
+  <div className={css.container}>
     <h1>InvoiceX</h1>
     <InvoiceList />
   </div>
